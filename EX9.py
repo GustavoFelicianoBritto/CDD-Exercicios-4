1d9a2c008a9a540f8d2fdd963d782f05326b75ef
GustavoFelicianoBritto <gustavofelicianoemail@gmail.com>
2025-04-17 18:18:05 -0300

    correção

--- EX9.py
+++ EX9.py
@@ -1,6 +1,6 @@
 num = int(input("Digite um número: "))
 
 for i in range (1,num+1,1):
-   for j in range(0,i,1):
-       print(f"{i}",end=" ")
-   print()
+    for j in range(0,i,1):
+        print(f"{i}",end=" ")
+    print()
